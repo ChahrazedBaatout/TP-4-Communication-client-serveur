2863eff84bb7b4a9ce9416c5482211a8a26d669a
HADJ SASSI Mahdi <mahdihadjsassi1@gmail.com>
2025-11-05 09:00:30 +0100

    Prepare the array and its function to test

--- client.c
+++ client.c
@@ -1,9 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "segdef.h"
+#define ARRAY_SIZE 10
+
+long* generateRandomArray(int size) {
+    long* arr = (long*)malloc(size * sizeof(long));
+    for (int i = 0; i < size; i++) {
+        arr[i] = getrand();
+    }
+    return arr;
+}
+
+void displayArray(long* arr, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%ld ", arr[i]);
+    }
+    printf("\n");
+}
+
+long averageArray(long* arr, int size) {
+    long sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return sum / size;
+}
 
 int main(void) {
     init_rand();
-    long num = getrand();
-    printf("Generated randomly number : %ld \n", num);
+    int size = ARRAY_SIZE;
+    long* randomArray = generateRandomArray(size);
+    printf("Generated array: ");
+    displayArray(randomArray, size);
+    printf("Average: %ld\n",  averageArray(randomArray, size));
     return 0;
 }
