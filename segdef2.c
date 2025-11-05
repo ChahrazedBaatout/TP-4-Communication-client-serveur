4666ff50c028f525991ae8f913ac01c758d43c35
Chahrazed baatout <baatoutchahrazed@gmail.com>
2025-11-05 11:58:31 +0100

    redefining library segdef

---
+++ segdef2.c
@@ -0,0 +1,48 @@
+#include "segdef2.h"
+#include <stdio.h>
+
+#include <stdlib.h>
+#include <time.h>
+
+void init_rand() {
+    srand(time(NULL));
+}
+long getrand() {
+    return rand();
+}
+
+void wait_sem(int id, int sem){
+
+    struct sembuf sop;
+    sop.sem_flg = SEM_UNDO;
+    sop.sem_num = sem;
+    sop.sem_op = 0; //waits sem 0
+
+    if(semop(id, &sop, 1) == -1){
+        perror("semop");
+        exit(1);
+    }
+}
+void acq_sem(int id, int sem) {
+    struct sembuf sop;
+    sop.sem_flg = SEM_UNDO;
+    sop.sem_num = sem;
+    sop.sem_op = -1; //acquires sem
+
+    if(semop(id, &sop, 1) == -1){
+        perror("semop");
+        exit(1);
+    }
+}
+void lib_sem(int id, int sem){
+
+    struct sembuf sop;
+    sop.sem_flg = SEM_UNDO;
+    sop.sem_num = sem;
+    sop.sem_op = 1; //frees sem 0
+
+    if(semop(id, &sop, 1) == -1){
+        perror("semop");
+        exit(1);
+    }
+}
